297e92717ba4d603244fabac5f41bbbd23df28e6
lakshmipriya-ns <lakshmipriyansns@gmail.com>
2023-03-23 16:59:51 +0530

    changes

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import Home from './components/Home';
+import Home from '../components/Home';
 import '@testing-library/jest-dom'
 
 describe('Home Test', () => {
+
+  test('Home Page', ()=>{
+   const welcome=render(<Home/>);
+   expect(welcome).toBeTruthy();
+  })
+
+
   test('header renders with welcome to home page', () => {
     render(<Home />);
     const linkElement = screen.getByText(/welcome to home page/i);
     expect(linkElement).toBeInTheDocument();
   });
+
+  
 })
 
 
